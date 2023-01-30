badedfc8ac83c648a394f04ac09c2ed0bfd06f63
Gilmer Contreras <adgdiux@gmail.com>
2023-01-30 18:35:02 -0400

    ARREGLOS 30012023 BACKEND

--- middlewares/validar-subscriber.js
+++ middlewares/validar-subscriber.js
@@ -21,19 +21,64 @@ async function validateDate() {
 
             for (const subscriber of subscribers) {
 
-                const body = { "IsDisabled": true };
-
-                const response = await fetch(`${subscriber.server.url}/emby/Users/${subscriber.uid}/Policy`, {
-                    method: 'post',
-                    body: JSON.stringify(body),
+                // OBTENER USER DE EMBY
+                const userDB = await fetch(`${subscriber.server.url}/emby/Users/${subscriber.uid}`, {
+                    method: 'GET',
                     headers: {
-                        'Content-Type': 'application/json',
                         "X-Emby-Token": subscriber.server.apikey,
                     }
                 });
-                const data = await response;
 
-                await Subscription.findByIdAndUpdate(subscriber._id, { status: false }, { new: true, useFindAndModify: false })
+                userDB.json().then(async user => {
+
+                        if (user.Policy.IsAdministrator) {
+
+                            let body = {
+                                "IsDisabled": false,
+                                "IsAdministrator": true
+                            };
+
+                            const response = await fetch(`${subscriber.server.url}/emby/Users/${subscriber.uid}/Policy`, {
+                                method: 'post',
+                                body: JSON.stringify(body),
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    "X-Emby-Token": subscriber.server.apikey,
+                                }
+                            });
+
+                            // const data = await response.json().then(async() => {
+
+                            //     const response = await fetch(`${subscriber.server.url}/emby/Users/${subscriber.uid}/Policy`, {
+                            //         method: 'post',
+                            //         body: JSON.stringify(body),
+                            //         headers: {
+                            //             'Content-Type': 'application/json',
+                            //             "X-Emby-Token": subscriber.server.apikey,
+                            //         }
+                            //     });
+
+                            // });
+
+                            await Subscription.findByIdAndUpdate(subscriber._id, { status: false }, { new: true, useFindAndModify: false })
+                        } else {
+
+                            const response = await fetch(`${subscriber.server.url}/emby/Users/${subscriber.uid}/Policy`, {
+                                method: 'post',
+                                body: JSON.stringify({ "IsDisabled": false }),
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    "X-Emby-Token": subscriber.server.apikey,
+                                }
+                            });
+
+                            await Subscription.findByIdAndUpdate(subscriber._id, { status: false }, { new: true, useFindAndModify: false })
+
+                        }
+
+                    })
+                    // OBTENER USER DE EMBY
+
 
             }
 
